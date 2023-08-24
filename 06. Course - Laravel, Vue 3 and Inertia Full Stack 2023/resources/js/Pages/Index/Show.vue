f2f893ca1fc85fffe570e44c8de56bf0d02f2329
rafaelRavelliIopay <rafael.ravelli@iopay.com.br>
2023-08-24 11:25:20 -0300

    06. course - class 04.003 done

--- 06. Course - Laravel, Vue 3 and Inertia Full Stack 2023/resources/js/Pages/Index/Show.vue
+++ 06. Course - Laravel, Vue 3 and Inertia Full Stack 2023/resources/js/Pages/Index/Show.vue
@@ -1,11 +1,14 @@
 <template>
-	<MainLayout>
-		<div>Show</div>
-		<Link href="/">Main Page</Link>
-	</MainLayout>
+	<div>Show</div>
+	<Link href="/">Main Page</Link>
 </template>
   
 <script setup>
 import { Link } from '@inertiajs/vue3'
-import MainLayout from '../../Layouts/MainLayout.vue'
 </script>
+<!-- <script>
+import MainLayout from '../../Layouts/MainLayout.vue';
+export default {
+	layout: MainLayout
+}
+</script> -->
--- 06. Course - Laravel, Vue 3 and Inertia Full Stack 2023/resources/js/app.js
+++ 06. Course - Laravel, Vue 3 and Inertia Full Stack 2023/resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import '../css/app.css';
+import MainLayout from './Layouts/MainLayout.vue'
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
@@ -10,7 +11,13 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    // resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: async (name) => {
+        const pages = import.meta.glob('./Pages/**/*.vue');
+        const page = await pages[`./Pages/${name}.vue`]();
+        page.default.layout = page.default.layout || MainLayout;
+        return page;
+    },
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
